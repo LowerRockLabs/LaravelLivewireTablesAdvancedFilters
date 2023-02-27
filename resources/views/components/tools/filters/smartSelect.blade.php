d87bdbd388960bf536d7b076a9e31ecaa64e836d
Joe <joe@lowerrocklabs.com>
2023-02-27 19:42:22 +0000

    SmartSelect Fixes

--- resources/views/components/tools/filters/smartSelect.blade.php
+++ resources/views/components/tools/filters/smartSelect.blade.php
@@ -1,44 +1,64 @@
 @php
     $theme = $component->getTheme();
     $filterKey = $component->getTableName() . '.filters.' . $filter->getKey();
+    $selectedItems = !empty($component->getAppliedFilterWithValue($filter->getKey())) ? $component->getAppliedFilterWithValue($filter->getKey()) : [];
 @endphp
 
 @if ($theme === 'tailwind')
     <div class="h-12 -mr-4 relative" x-data="{
         search: '',
-        open: false,
+        smartSelectOpen: false,
         checkedItems: [],
-        selectedItems: $wire.get('{{ $component->getTableName() }}.filters.{{ $filter->getKey() }}'),
-        items: @js($filter->getOptions()),
-        get filteredItems() {
-            filteredList = this.items;
-            return filteredList.filter(
-                i => i.name.toLowerCase().startsWith(this.search.toLowerCase())
-            )
+        currentFilteredList: [],
+        filteredList: [],
+        tmp: {{ json_encode($selectedItems) }},
+        selectedItems: {{ json_encode($selectedItems) }},
+        items: {{ json_encode($filter->getOptions()) }},
+        setupCurrentFilteredList() {
+            this.filteredList = Array.from(new Set({{ json_encode($filter->getOptions()) }}));
+            updateCurrentFilteredList();
         },
-        pushToSelected(itemID) {
-            this.checkedItems.push(itemID);
-            this.checkedItems = Array.from(new Set(this.checkedItems));
+        updateCurrentFilteredList() {
+            if (this.filteredList.length === 0) {
+                this.filteredList = Array.from(new Set({{ json_encode($filter->getOptions()) }}));
+            }
     
-        },
-        updateLivewire() {
-            this.open = false;
-            this.checkedItems = Array.from(new Set(this.checkedItems));
-            this.checkedItems.sort();
-    
-            tmp = $wire.get('{{ $component->getTableName() }}.filters.{{ $filter->getKey() }}');
-            if (tmp != this.checkedItems) {
-                $wire.set('{{ $component->getTableName() }}.filters.{{ $filter->getKey() }}', this.checkedItems);
+            if ($refs.smartSelectSearchBox.value != '') {
+                this.currentFilteredList = this.filteredList.filter(
+                    i => i.name.toLowerCase().startsWith($refs.smartSelectSearchBox.value)
+                );
             }
         },
-    }" @click.outside="updateLivewire()">
+        getCurrentFilteredList() {
+            if (this.filteredList.length === 0) {
+                this.filteredList = Array.from(new Set({{ json_encode($filter->getOptions()) }}));
+            }
+            return this.filteredList.filter(
+                i => i.name.toLowerCase().startsWith($refs.smartSelectSearchBox.value)
+            );
+        },
+        pushToSelected(itemID) {
+            this.checkedItems = Array.from(new Set(this.selectedItems));
+            this.checkedItems.push(itemID);
+            this.currentFilteredList = []
+            $refs.smartSelectSearchBox.value = '';
+            $wire.set('{{ $filterKey }}', this.checkedItems);
+    
+        },
+        init() {
+            if (this.filteredList.length === 0) {
+                this.filteredList = Array.from(new Set(this.items));
+            }
+            this.currentFilteredList = [];
+        },
+    }">
         <div class=" w-full flex flex-col items-center justify-center">
-            <input x-on:keydown="open = true" type="search" x-model="search" placeholder="Search Here..."
+            <input x-on:keyup="updateCurrentFilteredList" type="text" x-ref="smartSelectSearchBox"
+                placeholder="Search Here..."
                 class=" w-full mr-4	border-gray-300 rounded-md dark:bg-gray-800 dark:text-white dark:border-gray-600">
             <div class="flex-col w-full pr-4 ">
-
-                <ul x-show="open" class="bg-white dark:bg-darker flex-col w-full z-50">
-                    <template x-for="filteredItem in filteredItems" :key="filteredItem.id">
+                <ul class="bg-white dark:bg-darker flex-col w-full z-50">
+                    <template x-for="filteredItem in currentFilteredList" :key="filteredItem.id">
                         <li class="w-full flex text-gray-700 px-2 mt-2 text-black dark:text-white bg-white hover:bg-sky-700 dark:bg-darker hover:dark:bg-sky-700"
                             x-on:click="pushToSelected(filteredItem.id)">
                             <span>T</span>
